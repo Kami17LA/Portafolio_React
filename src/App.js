bf8178a7a2b9bb15418a6414e6770f7f936f7376
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-01-19 14:03:44 -0500

    Added the main description in Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout/index_la';
+import Home from './components/Home/index_ho'
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Home/>} />
+      </Route>
     </Routes>
     </>
   );
--- src/components/Home/index_ho.js
+++ src/components/Home/index_ho.js
@@ -1,1 +1,23 @@
 import './index_ho.scss'
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+
+    return(
+        <div className='container home-page' >
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm   
+                <img src={ LogoTitle } alt="developer" />
+                @therine
+                <br/>
+                Web developer
+                </h1>
+                <h2>Frontend Developer / Javascript / Project Managment</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
